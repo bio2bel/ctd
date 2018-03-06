c5b02a9a87504e8a7cdeed58e6319f0b333fe05a
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-03-06 22:56:23 +0100

    Update CLI

--- src/bio2bel_ctd/cli.py
+++ src/bio2bel_ctd/cli.py
@@ -6,8 +6,9 @@ import logging
 
 import click
 
-from bio2bel_ctd import Manager
-from bio2bel_ctd.constants import DEFAULT_CACHE_CONNECTION
+from .constants import DEFAULT_CACHE_CONNECTION
+from .manager import Manager
+from .models import Chemical, Gene
 
 
 @click.group()
@@ -53,6 +54,102 @@ def web(manager, debug, host, port):
     from .web import get_app
     app = get_app(connection=manager, url='/')
     app.run(debug=debug, host=host, port=port)
+
+
+@main.group()
+def manage():
+    pass
+
+
+@manage.group()
+def chemicals():
+    pass
+
+
+@chemicals.command()
+@click.argument('mesh_id')
+@click.pass_obj
+def get(manager, mesh_id):
+    """Get a chemical by its MeSH identifier. Try MESH:C490728 for lapatinib"""
+    chemical = manager.get_chemical_by_mesh(mesh_id)
+
+    if chemical is None:
+        click.echo('Not found: {}'.format(mesh_id))
+    else:
+        click.echo('MeSH Identifier: {}'.format(chemical.chemical_id))
+        click.echo('Name: {}'.format(chemical.chemical_name))
+        if chemical.definition:
+            click.echo('Definition: {}'.format(chemical.definition))
+
+
+@chemicals.command()
+@click.option('--limit', type=int, default=5)
+@click.option('--offset', type=int)
+@click.pass_obj
+def ls(manager, limit, offset):
+    query = manager.session.query(Chemical)
+
+    if limit > 0:
+        query = query.limit(limit)
+
+    if offset is not None:
+        query = query.offset(offset)
+
+    click.echo('\t'.join(('MeSH', 'Name', 'Definition', 'Parents')))
+
+    for chemical in query:
+        click.echo('\t'.join(map(str, (
+            chemical.chemical_id,
+            chemical.chemical_name,
+            chemical.definition,
+            '|'.join(map(str, chemical.parent_ids))
+        ))))
+
+
+@manage.group()
+def genes():
+    pass
+
+
+@genes.command()
+@click.argument('entrez_id')
+@click.pass_obj
+def get(manager, entrez_id):
+    """Get a gene by its Entrez Gene identifier"""
+    gene = manager.get_gene_by_entrez_id(entrez_id)
+
+    if gene is None:
+        click.echo('Not found: {}'.format(entrez_id))
+    else:
+        click.echo('Entrez Gene Identifier: {}'.format(gene.gene_id))
+        click.echo('Name: {}'.format(gene.gene_name))
+        click.echo('Symbol: {}'.format(gene.gene_symbol))
+
+        for ixn in gene.chemical_interactions.limit(5):
+            click.echo(ixn)
+
+
+@genes.command()
+@click.option('--limit', type=int, default=5)
+@click.option('--offset', type=int)
+@click.pass_obj
+def ls(manager, limit, offset):
+    query = manager.session.query(Gene)
+
+    if limit > 0:
+        query = query.limit(limit)
+
+    if offset is not None:
+        query = query.offset(offset)
+
+    click.echo('\t'.join(('EGID', 'Name', 'Symbol')))
+
+    for gene in query:
+        click.echo('\t'.join(map(str, (
+            gene.gene_id,
+            gene.gene_name,
+            gene.gene_symbol,
+        ))))
 
 
 if __name__ == '__main__':
