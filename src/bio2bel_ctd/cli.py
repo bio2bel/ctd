740c84a391da10436118e3f85842e69175f29892
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-05-11 13:37:09 +0200

    Update CLI

--- src/bio2bel_ctd/cli.py
+++ src/bio2bel_ctd/cli.py
@@ -2,60 +2,14 @@
 
 """Run this script with :code:`python3 -m bio2bel_ctd`"""
 
-import logging
 import sys
 
 import click
 
-from .constants import DEFAULT_CACHE_CONNECTION
 from .manager import Manager
 from .models import Action, Chemical, Gene
 
-
-@click.group()
-@click.option('-c', '--connection', help='Defaults to {}'.format(DEFAULT_CACHE_CONNECTION))
-@click.pass_context
-def main(ctx, connection):
-    """Convert CTD to BEL"""
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
-    logging.getLogger('bio2bel.utils').setLevel(logging.ERROR)
-    ctx.obj = Manager(connection=connection)
-
-
-@main.command()
-@click.pass_obj
-def populate(manager):
-    """Populates the database"""
-    manager.populate()
-
-
-@main.command()
-@click.option('-y', '--yes', is_flag=True)
-@click.pass_obj
-def drop(manager, yes):
-    """Drops database"""
-    if yes or click.confirm('Drop everything?'):
-        manager.drop_all()
-
-
-@main.command()
-@click.pass_obj
-def summarize(manager):
-    """Summarizes the database"""
-    for k, v in manager.summarize().items():
-        click.echo('{}: {}'.format(k.replace('_', ' ').title(), v))
-
-
-@main.command()
-@click.option('-v', '--debug', is_flag=True)
-@click.option('-h', '--host')
-@click.option('-p', '--port', type=int)
-@click.pass_obj
-def web(manager, debug, host, port):
-    """Run the web app"""
-    from .web import get_app
-    app = get_app(connection=manager, url='/')
-    app.run(debug=debug, host=host, port=port)
+main = Manager.get_cli()
 
 
 @main.group()
@@ -235,13 +189,6 @@ def ls(manager):
             action.description,
             action.parent_code
         )
-
-
-@main.command()
-@click.pass_obj
-def upload_bel(manager):
-    """Upload BEL to database"""
-    manager.upload_bel_graph()
 
 
 if __name__ == '__main__':
